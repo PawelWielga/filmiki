c8c4602a954c20c1d90507c1b0f21f17f3373dad
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2025-07-11 08:32:53 +0200

    Convert static site to React

--- player.js
+++ player.js
@@ -1,7 +1,18 @@
-const urlParams = new URLSearchParams(window.location.search);
-const videoFile = urlParams.get('video');
-if (videoFile) {
-  document.getElementById('player').src = videoFile;
-} else {
-  document.body.innerHTML = '<p style="color:white;">Nie znaleziono pliku wideo.</p>';
+function Player() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const videoFile = urlParams.get('video');
+
+  if (!videoFile) {
+    return <p style={{ color: 'white' }}>Nie znaleziono pliku wideo.</p>;
+  }
+
+  return (
+    <div className="container text-center">
+      <video controls autoPlay className="video-player" src={videoFile}>
+        Przeglądarka nie obsługuje wideo.
+      </video>
+    </div>
+  );
 }
+
+ReactDOM.render(<Player />, document.getElementById('root'));
